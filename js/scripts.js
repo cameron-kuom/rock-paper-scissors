26b1f0a224fd4159a2455a8642a048231ac2ba2c
cameron-kuom <117031925+cameron-kuom@users.noreply.github.com>
2022-11-26 14:19:02 -0500

    Create playRound function

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,26 @@
 function getComputerChoice(){
     let randomResult = Math.ceil(Math.random()*9)
     if (randomResult <= 3){
-        return "Rock";
+        return "rock";
     } else if (randomResult <= 6){
-        return "Paper";
+        return "paper";
     } else {
-        return "Scissors";
+        return "scissors";
     }
 }
+
+function playRound(playerSelection, computerSelection){
+    if (playerSelection == "rock"){
+        return computerSelection == "scissors" ? "You win!" : computerSelection == "paper" ? "You lose!" : "You tied!"
+    } else if (playerSelection == "paper"){
+        return computerSelection == "rock" ? "You win!" : computerSelection == "scissors" ? "You lose!" : "You tied!"
+    } else if (playerSelection == "scissors"){
+        return computerSelection == "paper" ? "You win!" : computerSelection == "rock" ? "You lose!" : "You tied!"
+    } else {
+        return "That is not an option, try again."
+    }
+}
+
+let playerSelection = window.prompt("Rock, paper, or scissors?").toLowerCase();
+let computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
