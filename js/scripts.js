9958c49f43109d575362ee628284ad6cb348480e
cameron-kuom <117031925+cameron-kuom@users.noreply.github.com>
2022-12-18 17:51:32 -0500

    Add roundResultContainer

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 const btnContainer = document.querySelector(".btn-container");
 const buttons = document.querySelectorAll("#btn");
-const roundResult = document.querySelector("#roundResult");
+const roundResultContainer = document.querySelector(".roundResult-container");
+const roundResult = document.createElement("h2");
 const pScore = document.querySelector("#pScore")
 const cScore = document.querySelector("#cScore");
 let playerSelection;
@@ -11,18 +12,22 @@ let computerScore = 0;
 function getComputerChoice(){
     let randomResult = Math.ceil(Math.random()*9)
     if (randomResult <= 3){
-        return "Rock";
+        return "rock";
     } else if (randomResult <= 6){
-        return "Paper";
+        return "paper";
     } else {
-        return "Scissors";
+        return "scissors";
     }
 }
 
 function playRound(playerSelection, computerSelection){
-    if (playerSelection == "Rock" && computerSelection == "Scissors" || playerSelection == "Paper" && computerSelection == "Rock" || playerSelection == "Scissors" && computerSelection == "Paper"){
+    if (playerSelection == "rock" && computerSelection == "scissors" || 
+    playerSelection == "paper" && computerSelection == "rock" || 
+    playerSelection == "scissors" && computerSelection == "paper"){
         return `Winner! You chose ${playerSelection} and the computer chose ${computerSelection}!`;
-    } else if (playerSelection == "Rock" && computerSelection == "Paper" || playerSelection == "Paper" && computerSelection == "Scissors" || playerSelection == "Scissors" && computerSelection == "Rock"){
+    } else if (playerSelection == "rock" && computerSelection == "paper" || 
+    playerSelection == "paper" && computerSelection == "scissors" || 
+    playerSelection == "scissors" && computerSelection == "rock"){
         return `Whoops! The computer chose ${computerSelection} and you chose ${playerSelection}.`;
     } else if (playerSelection == computerSelection){
         return `Try again. You both chose ${playerSelection}.`;
@@ -32,18 +37,17 @@ function playRound(playerSelection, computerSelection){
 }
 
 buttons.forEach(button => button.addEventListener("click", () => {
-    playerSelection = button.textContent;
+    playerSelection = button.textContent.toLowerCase();
     computerSelection = getComputerChoice();
     roundResult.textContent = playRound(playerSelection, computerSelection);
+    roundResultContainer.appendChild(roundResult);
 
     if (roundResult.textContent == `Winner! You chose ${playerSelection} and the computer chose ${computerSelection}!`){
         playerScore++
     } else if (roundResult.textContent == `Whoops! The computer chose ${computerSelection} and you chose ${playerSelection}.`){
         computerScore++
     }
-
     
-
     pScore.textContent = `Player: ${playerScore}`;
     cScore.textContent = `Computer: ${computerScore}`;
 }));
