697c2001ebfefb3042e6d2d852986da9c9641903
cameron-kuom <117031925+cameron-kuom@users.noreply.github.com>
2022-12-18 13:03:48 -0500

    Add click event listener and intial css

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,8 @@
-const buttons = document.querySelectorAll("button");
+const btnContainer = document.querySelector(".btn-container");
+const buttons = document.querySelectorAll("#btn");
+const roundResult = document.querySelector("#roundResult");
+const pScore = document.querySelector("#pScore")
+const cScore = document.querySelector("#cScore");
 let playerSelection;
 let computerSelection;
 let playerScore = 0;
@@ -19,10 +23,27 @@ function playRound(playerSelection, computerSelection){
     if (playerSelection == "Rock" && computerSelection == "Scissors" || playerSelection == "Paper" && computerSelection == "Rock" || playerSelection == "Scissors" && computerSelection == "Paper"){
         return `Winner! You chose ${playerSelection} and the computer chose ${computerSelection}!`;
     } else if (playerSelection == "Rock" && computerSelection == "Paper" || playerSelection == "Paper" && computerSelection == "Scissors" || playerSelection == "Scissors" && computerSelection == "Rock"){
-        return `Whoops! THe computer chose ${computerSelection} and you chose ${playerSelection}.`;
+        return `Whoops! The computer chose ${computerSelection} and you chose ${playerSelection}.`;
     } else if (playerSelection == computerSelection){
         return `Try again. You both chose ${playerSelection}.`;
     } else {
         return `This is not an option. Try again.`;
     }
 }
+
+buttons.forEach(button => button.addEventListener("click", () => {
+    playerSelection = button.textContent;
+    computerSelection = getComputerChoice();
+    roundResult.textContent = playRound(playerSelection, computerSelection);
+
+    if (roundResult.textContent == `Winner! You chose ${playerSelection} and the computer chose ${computerSelection}!`){
+        playerScore++
+    } else if (roundResult.textContent == `Whoops! The computer chose ${computerSelection} and you chose ${playerSelection}.`){
+        computerScore++
+    }
+
+    
+
+    pScore.textContent = `Player: ${playerScore}`;
+    cScore.textContent = `Computer: ${computerScore}`;
+}));
