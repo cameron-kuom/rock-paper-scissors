a553ecc1c94f95b3926cf0dbabb62c56d2653d51
cameron-kuom <117031925+cameron-kuom@users.noreply.github.com>
2022-12-18 09:32:50 -0500

    Update HTML and remove game function

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,8 @@
+const buttons = document.querySelectorAll("button");
 let playerSelection;
 let computerSelection;
-const buttons = document.querySelectorAll("button");
-
-const roundMessage = [
-    "Congratulations, you won the round!",
-    "Sorry, you lost the round!",
-    "Try again, you both chose ",
-    "This is not an option, try again."
-];
+let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice(){
     let randomResult = Math.ceil(Math.random()*9)
@@ -22,55 +17,12 @@ function getComputerChoice(){
 
 function playRound(playerSelection, computerSelection){
     if (playerSelection == "Rock" && computerSelection == "Scissors" || playerSelection == "Paper" && computerSelection == "Rock" || playerSelection == "Scissors" && computerSelection == "Paper"){
-        return roundMessage[0];
+        return `Winner! You chose ${playerSelection} and the computer chose ${computerSelection}!`;
     } else if (playerSelection == "Rock" && computerSelection == "Paper" || playerSelection == "Paper" && computerSelection == "Scissors" || playerSelection == "Scissors" && computerSelection == "Rock"){
-        return roundMessage[1];
+        return `Whoops! THe computer chose ${computerSelection} and you chose ${playerSelection}.`;
     } else if (playerSelection == computerSelection){
-        return roundMessage[2] + playerSelection + ".";
+        return `Try again. You both chose ${playerSelection}.`;
     } else {
-        return roundMessage[3];
+        return `This is not an option. Try again.`;
     }
 }
-
-
-function game(){
-    let playerScore = 0;
-    let computerScore = 0;
-    let player = document.querySelector("#player");
-    let computer = document.querySelector("#computer");
-    let winner = document.querySelector("#winner");
-
-    for (let i = 0; i <= 5; i++){
-        playerSelection = buttons.forEach(button => button.addEventListener("click", () => {
-        
-        
-            getComputerChoice();
-            computerSelection = getComputerChoice();
-            playRound(playerSelection, computerSelection);
-            
-            player.textContent = `Player: ${playerSelection}`;
-            computer.textContent = `Computer: ${computerSelection}`;
-            winner.textContent = playRound(playerSelection, computerSelection)
-            
-            if (playRound(playerSelection, computerSelection) == roundMessage[0]){
-                playerScore++
-            } else if (playRound(playerSelection, computerSelection) == roundMessage[1]){
-                computerScore++
-            }
-        }));
-    };
-
-    if (playerScore > computerScore){
-        console.log("Congratulations! You've beaten the computer!")
-    } else if (computerScore > playerScore){
-        console.log("Sorry! The computer has won the game!")
-    } else {
-        console.log("Game over! We have a tie!")
-    }
-
-};
-    
-    
-    
-
-game();
